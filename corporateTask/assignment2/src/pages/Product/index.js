1b3ab3763bc0d94194764e3898ee89022ccef194
yeoncho <yeoncho@student.42seoul.kr>
2021-07-31 01:40:56 +0900

    ✨feat: Product 페이지 1차 구현

--- corporateTask/assignment2/src/pages/Product/index.js
+++ corporateTask/assignment2/src/pages/Product/index.js
@@ -1,17 +1,45 @@
 import { Component } from "react";
 import { withRouter } from "react-router";
 import Header from "../../components/Header";
+import PRODUCT_LIST from "utils/constants/PRODUCT_LIST";
+import checkForApathy from "utils/checkProduct/checkForApathy";
+import addApathy from "utils/manageLocalStorage/addApathy";
+import addInquiryHistory from "utils/manageLocalStorage/addInquiryHistory";
+import RandomButton from "components/RandomButton";
 
 class Product extends Component {
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    console.log(id);
+  constructor(props) {
+      super(props);
+      this.state = {
+          currentProductInfo: this.props.currentProductInfo,
+      };
   }
+  
+    getRandomProduct = (currentProductId) => {
+        while (true) {
+            let newProductId = Math.floor(Math.random() * (PRODUCT_LIST.length + 1))
+            if (newProductId === 0 || newProductId === currentProductId || checkForApathy(currentProductId))
+                continue;
+            return PRODUCT_LIST.find((e) => e.id === newProductId);
+        }
+    }
+
+    handleOnClickRandomButton = (isApathyButton) => {
+        addInquiryHistory(this.state.currentProductInfo);
+        const currentProductId = this.state.currentProductInfo.id;
+        if (isApathyButton) {
+            addApathy(currentProductId);
+        }
+        this.setState(prevState => ({currentProductInfo: this.getRandomProduct(currentProductId)}))
+    }
+
   render() {
     return (
       <div className="product-page">
         <Header />
         <div className="product-page-content">Product Page</div>
+        <RandomButton isApathyButton={false} handleOnClick={this.handleOnClickRandomButton}/>
+        <RandomButton isApathyButton={true} handleOnClick={this.handleOnClickRandomButton}/>
       </div>
     );
   }
