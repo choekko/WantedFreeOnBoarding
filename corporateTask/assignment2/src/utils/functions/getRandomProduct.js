282d7f0423164b949c6a2f403e50c058e7db0d54
yeoncho <yeoncho@student.42seoul.kr>
2021-07-30 23:14:53 +0900

    🔨fix: utils/functions 함수명 오타 및 import 함수명 오타 수정

--- corporateTask/assignment2/src/utils/functions/getRandomProduct.js
+++ corporateTask/assignment2/src/utils/functions/getRandomProduct.js
@@ -1,11 +1,11 @@
 import Product from "../../pages/Product"
 import PRODUCT_LIST from "../constants/PRODUCT_LIST"
-import isNotInterestedProducts from "./isNotInderested";
+import isNotInterested from "./isNotInderested";
 
 const getRandomProduct = (currentProductId) => {
     while (true) {
         let newProductId = Math.floor(Math.random() * 101)
-        if (newProductId === 0 || newProductId === currentProductId || isNotInterestedProducts(productId))
+        if (newProductId === 0 || newProductId === currentProductId || isNotInterested(productId))
             continue;
         return PRODUCT_LIST.find((e) => e.id === newProductId);
     }
